fa2cc415c62fec7d3a480055225956ae19913a14
Aqsaa Qaazi <159919683+GD-aish0o@users.noreply.github.com>
2024-05-13 18:49:04 +0500

    Add files via upload
    The piece of code in these files is a simple typescript loop by default calculating the sum of first 10 digits in counting. Read the code & let the author know what needs to be fixed.

---
+++ index.js
@@ -0,0 +1,5 @@
+var sum = 0;
+for (var i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log("The sum of the first 10 numbers is: ".concat(sum));
---
+++ index.ts
@@ -0,0 +1,20 @@
+// - Write a for loop to find the sum of the first 10 numbers.
+
+// here's a simple code
+
+
+let sum = 0;
+
+// feel free to edit * the number below this steroid.
+for (let i = 1; i <= 10; i++) {
+
+
+    // The += operator is used for assignment with addition. It combines the value on the left-hand side with the value on the right-hand side and assigns the result back to the left-hand side variable. 
+
+    // Take this as an example: 
+    // sum=5; 
+//  sum += 3; after this whenever you print the sun variable it will return 8. 
+    sum += i;
+}
+
+console.log(`The sum of the first 10 numbers is: ${sum}`);
